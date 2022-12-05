022e8c76b88583007d55762402e7557e97172d0f
psantacreu <pablosantacreu@yahoo.com.ar>
2022-12-05 11:14:13 -0300

    T11

---
+++ js/java.js
@@ -0,0 +1,61 @@
+
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+
+alert("Realiza 3 series de 10 abdominales.");
+
+break;
+case "martes":
+
+alert("Realiza 3 series de 10 sentadillas.");
+
+break;
+case "miércoles":
+
+alert("Realiza 3 series de 10 flexiones.");
+
+break;
+case "jueves":
+
+alert("Realiza 3 series de 10 salto de rana.");
+
+break;
+case "viernes":
+
+alert("Realiza 3 series de 3 min. de Tablas.");
+
+break;
+case "sábado":
+
+alert("Realiza 3 series de 3 min. sentado.");
+
+break;
+case "domingo":
+
+alert("Dia de descanzo");
+
+break;
+default:
+
+alert("Si escribes mal el día, será el doble de ejercicios. (solo minusculas)");
+
+}
+
+
+
+for(i=20;i<=70;i++){
+
+    document.write("El número es: " + i + "<br>");
+}
+
+let numero = prompt("ingresar número");
+
+for(i=0;i<numero;i++){
+
+    document.write("Hola Mundo " + "<br>");
+}
+
+
+
+
